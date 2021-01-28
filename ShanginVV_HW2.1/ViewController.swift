d5b12025c1d4ae7559e11138e4a65fca2b708573
Shangin Vyacheslav <slava-poison@yandex.ru>
2021-01-28 08:58:34 +0400

    commit 1

---
+++ ShanginVV_HW2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShanginVV_HW2.1
+//
+//  Created by Вячеслав Шангин on 28.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any a dditional setup after loading the view.
+    }
+
+
+}
+
